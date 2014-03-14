94dc7942c78ca1afada89a1a5be42201a6007215
Otavio Fernandes <otavio@neton.com.br>
2014-03-14 15:00:56 -0300

    Fix

--- tests/index.php
+++ tests/index.php
@@ -8,9 +8,9 @@
 
 require_once __DIR__."/../vendor/autoload.php";
 
-$usuario = "api@dexto.com.br";
-$senha = "api181818";
-$conta = "dextotec";
+$usuario = "";
+$senha = "";
+$conta = "";
 //error_reporting(~E_NOTICE);
 $api = new \Superlogica\Api('https://superlogica.net/financeiro/atual');
 $api->login($usuario, $senha, $conta);
